2e5e08dcaf5ca484739badad9b091b2a3e196c30
Maxence Maze au <maxencemazeau@Maxences-MacBook-Air.local>
2023-08-11 18:06:54 +0200

    prot change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
